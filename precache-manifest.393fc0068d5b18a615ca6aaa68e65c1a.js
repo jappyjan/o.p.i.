91d9e38b8d1108bbeba1c6687644734359c11ef2
Jan Jaap <mail@janjaap.de>
2021-05-22 00:29:03 +0200

    gh-pages

---
+++ precache-manifest.393fc0068d5b18a615ca6aaa68e65c1a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9830e6d2300a27e5a0",
+    "url": "/o.p.i./css/app.321ec5b2.css"
+  },
+  {
+    "revision": "d726ed28d900a21fa902",
+    "url": "/o.p.i./css/chunk-vendors.97bf8a5e.css"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/o.p.i./fonts/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/o.p.i./fonts/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/o.p.i./fonts/primeicons.b0f5d02f.eot"
+  },
+  {
+    "revision": "15431381890720a5a4b62b33c8ae06c5",
+    "url": "/o.p.i./fonts/roboto-v20-latin-ext_latin-500.15431381.woff2"
+  },
+  {
+    "revision": "3d9446f210892af971a3854d0f984ae9",
+    "url": "/o.p.i./fonts/roboto-v20-latin-ext_latin-500.3d9446f2.woff"
+  },
+  {
+    "revision": "52bb58c8cb04cf3eea4f9ac0afa1d1f6",
+    "url": "/o.p.i./fonts/roboto-v20-latin-ext_latin-700.52bb58c8.woff2"
+  },
+  {
+    "revision": "d0cc855e64aca1072711f49d1a38ca0c",
+    "url": "/o.p.i./fonts/roboto-v20-latin-ext_latin-700.d0cc855e.woff"
+  },
+  {
+    "revision": "c5bf51b68dc9fd7fe944d8947fe12518",
+    "url": "/o.p.i./fonts/roboto-v20-latin-ext_latin-regular.c5bf51b6.woff2"
+  },
+  {
+    "revision": "d679a90608a65cc9394f67448ca2094a",
+    "url": "/o.p.i./fonts/roboto-v20-latin-ext_latin-regular.d679a906.woff"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/o.p.i./img/color.c7a33805.png"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/o.p.i./img/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "bd349c8d8b3bd3caf17b70200b53b659",
+    "url": "/o.p.i./index.html"
+  },
+  {
+    "revision": "1c9830e6d2300a27e5a0",
+    "url": "/o.p.i./js/app.dee3a3cb.js"
+  },
+  {
+    "revision": "d726ed28d900a21fa902",
+    "url": "/o.p.i./js/chunk-vendors.daa96321.js"
+  },
+  {
+    "revision": "252d95f3e46935478579",
+    "url": "/o.p.i./js/locale-en-json.381d48fd.js"
+  },
+  {
+    "revision": "0fb67ed7e6816ff30d20c2217e37af18",
+    "url": "/o.p.i./manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/o.p.i./robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/o.p.i./precache-manifest.393fc0068d5b18a615ca6aaa68e65c1a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "o.p.i."});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
